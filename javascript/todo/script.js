3c98dd4f5284219a022ef3399da1d0e42d6f97b2
FrançoisB <briandfrancois@yahoo.fr>
2020-01-23 16:57:34 +0100

    Exercice Todo List Final

--- javascript/todo/script.js
+++ javascript/todo/script.js
@@ -3,12 +3,45 @@ var tasks = document.querySelector('#tasks');
 
 taskText.addEventListener('keyup', function (event) {
     if (event.keyCode === 13 && taskText.value) {
-        var text = document.createTextNode(taskText.value);
-        // var li = '<li>' + text + '<li>';
-        var li = document.createElement('li');
-        li.appendChild(text);
-        tasks.appendChild(li);
+        var node = createTaskNode(taskText.value);
+
+        tasks.appendChild(node);
+        taskText.value='';
 
         
     }
 });
+tasks.addEventListener('click',function(event){
+    var elem=event.target;
+    if(elem.classList.contains('remove')){
+
+    //    tasks.removeChild(elem.parentNode);
+    elem.parentNode.remove();
+
+    }
+    if(elem.classList.contains('done')){
+        elem.classList.toggle('toggle-done');
+    }
+    
+});
+
+function createTaskNode(txt){
+    var text = document.createTextNode(txt +' ');
+    // var li = '<li>' + text + '<li>';
+    var li = document.createElement('li');
+    var span=document.createElement('span');
+    var i=document.createElement('i');
+
+    li.appendChild(text);
+    tasks.appendChild(li);
+
+    i.classList.add('fa');
+    i.classList.add('fa-trash');
+
+
+    li.appendChild(text);
+    li.appendChild(span);
+    li.appendChild(i);
+
+    return li;
+}
